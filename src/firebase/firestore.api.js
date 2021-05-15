12b4fcb849ac58409f7eedd805900cd348ec1090
JEKREAL <jek_real@yahoo.com>
2021-05-15 16:54:40 +0300

    getUser,getVenue

--- src/firebase/firestore.api.js
+++ src/firebase/firestore.api.js
@@ -1,10 +1,28 @@
-// collections "users", "venues"
-// doc === uid 
+import firebase from "firebase/app";
+// import { GetUsers, GetVenues } from "../firebase/firestore.api";  
+// // collections "users", "venues"
+// // doc === uid 
 
 //function to get user info
-
+export async function  GetUsers() {  
+    firebase.firestore()
+        .collection("users")
+        .get()
+        .then(snap => {
+            return snap.docs.map(user => user.data());
+        }).then(users => {
+            console.log(users)
+        })
+}
 
 //function to get venue info and return 
-function getinfo(){
-
+export async function GetVenues() {
+    firebase.firestore()
+        .collection("venues")
+        .get()
+        .then(snap => {
+            return snap.docs.map(venue => venue.data());
+        }).then(venues => {
+            console.log(venues)
+        })
 }
