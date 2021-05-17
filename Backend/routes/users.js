6b91f9e964a4a821164bb1d763dfafa658a29c62
JEKREAL <jek_real@yahoo.com>
2021-05-17 11:51:39 +0300

    added adduser,getusers endpoints, some validation

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -2,6 +2,9 @@ var express = require("express");
 var router = express.Router();
 const admin = require("firebase-admin");
 const serviceAccount = require("../serviceAccountKey.json");
+const { userCreationValidators } = require('../validation');
+const { body, validationResult } = require('express-validator')
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
@@ -43,12 +46,26 @@ router.post("/sessionLogin", async (req, res) => {
   res.send(userData.data());
 });
 
-module.exports = router;
-
 router.post("/sessionSignUp", async (req, res) => {
   // const idToken = req.body.idToken
   // const verify = await admin.auth().verifyIdToken(idToken)
   res.send({ msg: "Signed up successfully" });
+});
+
+router.get("/", async (req, res) => { //get users
+  const user = await admin.firestore().collection("users").get()
+  const users = await user.docs.map(user => user.data())
+  res.send(users);
+});
+
+router.post("/",userCreationValidators, async (req, res) => { //add user
+  const errors = validationResult(req)
+  if(!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()})
+  }
+  const newUser = req.body;
+  await admin.firestore().collection("users").add(newUser) 
+  res.send({msg: "User added successfully"});
 });
 
 module.exports = router;
