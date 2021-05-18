5648d8b124cd00008ccb52d74086055bc094c66a
Yeonathan Aizenberg <afula.aizenberg@gmail.com>
2021-05-18 15:53:24 +0300

    Path to DemoPage

---
+++ src/components/DemoPage.jsx
@@ -0,0 +1,11 @@
+const DemoPage = () => {
+    return (
+        <>
+        <div className="fs-2 text-start m-5" style={{ color: "lightsteelblue" }}>
+                Demo Page
+        </div>
+        </>
+    );
+};
+
+export default DemoPage;
