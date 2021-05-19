1dbab428328f6d1fd4cdcd42af880b5d9bfd843c
JEKREAL <jek_real@yahoo.com>
2021-05-19 12:52:21 +0300

    sign_in val

--- src/components/SignInEmailModal.jsx
+++ src/components/SignInEmailModal.jsx
@@ -3,31 +3,44 @@ import { Button, Modal, Form } from "react-bootstrap";
 import { useAuth } from "../context/auth";
 import { signInEmail } from "../firebase/firebase.auth";
 import React, { useEffect } from 'react'
+import validateSignIn from './validate';
 
 export default function SignInEmailModal() {
   const [open, setOpen] = useState(false);
   const [pwd, setpwd] = useState(null);
   const [mail, setmail] = useState(null);
   const { saveToken } = useAuth();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const userInfo = await signInEmail(mail, pwd);
-    saveToken(userInfo);
-  };
+  const [errors, setErrors] = useState({});
 
   useEffect(() => {
     document.body.addEventListener('keydown', closeOnEscapeKeyDown)
     return async function cleanup() {
-       await document.body.removeEventListener('keydown', closeOnEscapeKeyDown)
+      await document.body.removeEventListener('keydown', closeOnEscapeKeyDown)
     }
-}, [])
+  }, [])
 
-const closeOnEscapeKeyDown = (e) => {
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const inputs = { email: mail, password: pwd }
+    const validationErrors = validateSignIn(inputs);
+    const noErrors = Object.keys(validationErrors).length === 0;
+    setErrors(validationErrors);
+    if (noErrors) {
+      const userInfo = await signInEmail(mail, pwd);
+      saveToken(userInfo);
+      console.log("Authenticated", inputs);
+    } else {
+      console.log("errors try again", validationErrors);
+    }
+
+  }
+
+  const closeOnEscapeKeyDown = (e) => {
     if ((e.charCode || e.keyCode) === 27) {
       setOpen(false)
     }
-}  
+  }
   return (
     <>
       <Button onClick={() => setOpen(true)}>Sign in Email</Button>
@@ -43,6 +56,7 @@ const closeOnEscapeKeyDown = (e) => {
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
+            {errors.email && <p style={{ color: 'red' }}>{errors.email}</p>}
           </Form.Group>
 
           <Form.Group controlId="formBasicPassword">
@@ -52,6 +66,7 @@ const closeOnEscapeKeyDown = (e) => {
               type="password"
               placeholder="Password"
             />
+            {errors.password && <p style={{ color: 'red' }}>{errors.password}</p>}
           </Form.Group>
           <Button variant="primary" type="submit" className="mt-2" block>
             Log in
