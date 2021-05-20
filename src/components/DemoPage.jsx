02402ab39e132eeb15d0a89396ee5223e9390120
Itay Levi <levietay@gmail.com>
2021-05-20 08:28:24 +0300

    Update DemoPage.jsx

--- src/components/DemoPage.jsx
+++ src/components/DemoPage.jsx
@@ -7,18 +7,24 @@ const DemoPage = () => {
 
     const [artistAvatar, setArtistAvatar] = useState(null);
     const [artistInfos, setArtistInfos] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
+        setLoading(true)
         const artistAva = "Artist Avatar" // get artist Avatar for DB
         const artistInfo = "Artist Infos" // get artist Infos for DB
         setArtistAvatar(artistAva)
         setArtistInfos(artistInfo)
+        setLoading(false)
     }, []);
 
     return (
         <>
         <div className="fs-2 text-start m-5" style={{ color: "lightsteelblue" }}>
                 <div>Demo Page</div>
+                {loading && <div class="spinner-border text-danger" role="status">
+  
+  </div>}
                 <span className="demoPageBaseInfo">
                     {/* Get the Artist avatar and contact info from DB */}
                     {artistAvatar && <img src="https://cdn0.iconfinder.com/data/icons/people-jobs-set-2/128/jobs-10-512.png" alt="Artist Avatar"/>}
